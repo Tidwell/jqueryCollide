ab009d5d45caec3303419af4c73e50c6bd2cbd08
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2011-01-21 05:21:19 +0000

    First ideas

---
+++ collidable.js
@@ -0,0 +1,27 @@
+﻿(function( $ ){
+
+  $.fn.collidable = function( options ) {  
+    //check what options is
+    //function, just setup the bindings
+    
+    //array, foreach and set up objects and bindings
+    
+    //object, extend with defaults and set up bindings
+    var defaults = {
+      'location'         : 'top',
+      'background-color' : 'blue'
+    };
+
+    return this.each(function() {        
+      // If options exist, lets merge them
+      // with our default settings
+      if ( options ) { 
+        $.extend( defaults, options );
+      }
+
+     
+
+    });
+
+  };
+})( jQuery );
